fa7cf000df19fcd005f8404c36eb1832c9149048
jack upt <jhonbahama@gmail.com>
2020-02-15 11:59:17 -0800

    JAWSDB added

--- models/burger.js
+++ models/burger.js
@@ -19,24 +19,3 @@ module.exports = function(sequelize, DataTypes){
     return Burger;
 };
 
-// var orm = require("../config/orm.js");
-
-// var burger = {
-//     selectAll: function (cb) {
-//         orm.selectAll("burgers", function (res) {
-//             cb(res);
-//         });
-//     },
-//     insertOne: function (cols, vals, cb) {
-//         orm.insertOne("burgers", cols, vals, function (res) {
-//             cb(res);
-//         });
-//     },
-//     updateOne: function (objColVals, condition, cb) {
-//         orm.updateOne("burgers", objColVals, condition, function (res) {
-//             cb(res);
-//         });
-//     },
-// };
-
-// module.exports = burger;
